d2e88d7760986affa31d93002a16a307b2058c2e
Vipul9953 <vipulkumar995318@gmail.com>
2024-04-21 11:24:01 +0530

    redis module and config

--- server/src/app.module.ts
+++ server/src/app.module.ts
@@ -2,9 +2,10 @@ import { Module } from '@nestjs/common';
 import { ConfigModule } from '@nestjs/config';
 import { PollsModule } from './polls/polls.module';
 import { App } from './app.controller';  
+import { redisModule } from './module.config';
 
 @Module({
-  imports: [ConfigModule.forRoot(), PollsModule],
+  imports: [ConfigModule.forRoot(), PollsModule, redisModule],
   controllers: [App], 
   providers: [],
 })
