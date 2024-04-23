53d6e0ad7afca064116fc23e8f2e0f727f4fda9d
Vipul9953 <vipulkumar995318@gmail.com>
2024-04-24 01:06:45 +0530

    work on redis

--- server/src/polls/polls.module.ts
+++ server/src/polls/polls.module.ts
@@ -2,10 +2,11 @@ import { Module } from '@nestjs/common';
 import { ConfigModule } from '@nestjs/config';
 import { PollsController } from './polls.controller';  
 import { PollsService } from './polls.service';
+import { PollsRepository } from './polls.repository';
 
 @Module({
   imports: [ConfigModule.forRoot()],
   controllers: [PollsController],
-  providers: [PollsService],
+  providers: [PollsService, PollsRepository],
 })
 export class PollsModule {}
