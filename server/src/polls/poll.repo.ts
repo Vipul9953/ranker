fc9359c264d91180f2a571f58036dcfdaed7c7a9
Vipul9953 <vipulkumar995318@gmail.com>
2024-04-24 01:13:38 +0530

    poll repo

---
+++ server/src/polls/poll.repo.ts
@@ -0,0 +1,19 @@
+import { Inject, Injectable, Logger } from "@nestjs/common";
+import { ConfigService } from "@nestjs/config";
+import { Redis } from "ioredis";
+import { IORedisKey } from "src/redis.module";
+
+
+
+@Injectable()
+export class PollRepo{
+    private readonly ttl: string;
+    private readonly logger = new Logger(PollsRepository.name);
+
+    constructor(
+        configService:ConfigService,
+        @Inject(IORedisKey) private readonly redisClient: Redis,
+    ){
+        this.ttl = configService.get('POLL_DURATION');
+    }
+}
